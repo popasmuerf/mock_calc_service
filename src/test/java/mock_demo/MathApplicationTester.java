1ba6c4c237b9872cd192c45776ce89497afda04b
mikeyb <MichaelBrown172@Gmail.com>
2019-07-02 14:52:24 -0400

    ....

--- src/test/java/mock_demo/MathApplicationTester.java
+++ src/test/java/mock_demo/MathApplicationTester.java
@@ -46,11 +46,15 @@ public class MathApplicationTester {
         //add the behavior of calc service to add two numbers
         when(calcService.add(10.0,20.0)).thenReturn(30.00);
 
+        //add the behavior of calc service to subtract two numbers..
+        when(calcService.subtract(10.0,20.0)).thenReturn(-10.0);
+
         //test the add functionality
         Assert.assertEquals(mathApplication.add(10.0, 20.0),30.0,0);
-
+        Assert.assertEquals(mathApplication.subtract(10.0,20.0), -10.0);
         //vefiry the behavior
         verify(calcService).add(10.0,20.0);
+        verify(calcService).subtract(10.0, 20.0);
     }
 
 
